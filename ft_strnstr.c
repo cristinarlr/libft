de27eb704f9d0d6c9d2a9d5e9bbc15b26a4a4270
Christina Ramirez <Cristina@Christinas-MacBook-Pro.local>
2023-03-16 00:22:06 +0100

    daily wrk: 15/03/23 libft: strnstr in progress

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,19 +6,39 @@
 /*   By: Cristina <Cristina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 23:57:13 by Cristina          #+#    #+#             */
-/*   Updated: 2023/03/16 00:02:41 by Cristina         ###   ########.fr       */
+/*   Updated: 2023/03/16 00:20:34 by Cristina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char * strnstr(const char *haystack, const char *needle, size_t len)
+/* char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+    // unsigned char *hay;
+    // unsigned char *ndl;
+
+    // hay = (unsigned char) haystack;
+    // ndl = (unsigned char) needle;
+    while (len > 0)
+        if (*haystack != *needle)
+        {
+            haystack++;
+            needle++;
+        }
+        while (*haystack == *needle && )
     return (0);
-}
+} */
 
 int main ()
 {
-    printf()
+    const char *hay;
+    const char *needle;
+    size_t     n;
+
+    n = 25;
+    hay = "soy un";
+    needle = "soy un gran pajar de palabras";
+
+    printf("%s\n", strnstr(hay, needle, n));
     return (0);
 }
