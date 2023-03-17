3098c8022c6246fcb5e499888b2e61a8117583df
Christina Ramirez <Cristina@Christinas-MacBook-Pro.local>
2023-03-17 14:17:02 +0100

    Daily work: 17/03/23 libft: ft_atoi

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ SRCS = 	ft_isalpha.c	\
 		ft_memchr.c		\
 		ft_memcmp.c		\
 		ft_strnstr.c	\
-		# ft_atoi.c		\
+		ft_atoi.c		\
 		# ft_calloc.c		\
 		# ft_strdup.c 	\
 		# ft_putnbr_fd.c	\
--- ft_atoi.c
+++ ft_atoi.c
@@ -3,33 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crramire <crramire@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Cristina <Cristina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/16 13:45:33 by crramire          #+#    #+#             */
-/*   Updated: 2023/03/16 14:21:02 by crramire         ###   ########.fr       */
+/*   Created: 2023/03/17 13:44:31 by Cristina          #+#    #+#             */
+/*   Updated: 2023/03/17 14:02:25 by Cristina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* It converts a string to an integer. */
-
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	char	*ptr;
-    int resultado;
+	size_t		i;
+	int			flag;
+	int			result;
 
-    resultado = *ptr - 78;
-    return (resultado);
+	flag = 1;
+	result = 0;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i++] == '-')
+			flag = -1;
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (0);
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + (str[i++] - '0');
+	return (result * flag);
 }
-
 
 int	 main ()
 {
 	char	*str;
+	int	value_atoi;
+	int	value_ft_atoi;
 
-	str = "3";
-	printf ("%d\n", atoi(str));
-	printf ("%d\n", ft_atoi(str));
+	str = "  -34 9";
+//	str = "123\b45";
+	value_atoi = atoi(str) - 2;
+	printf ("atoi num = %d\n", atoi(str));
+	printf ("atoi value of rest (%d - 2) = %d\n\n", atoi(str), value_atoi);
+	
+	value_ft_atoi = ft_atoi(str) - 2;
+	printf ("ft_atoi num = %d\n", ft_atoi(str));
+	printf ("ft_atoi value of rest (%d - 2) = %d\n", ft_atoi(str), value_ft_atoi);
+	
 	return (0);
 }
